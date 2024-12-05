9fd7458e5a629063bfb42b44e1d0c4c94b616c14
Aditya Ghadge <adityaghadge360@gmail.com>
2024-12-05 22:29:48 +0530

    add counselling scheduled section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,6 +10,18 @@ export default function Dashboard() {
       <div className="flex-1 py-8 px-12">
         <Header />
         <About />
+        <div className="bg-white rounded-[32px] p-8 mb-8">
+          <h2 className="text-2xl font-semibold mb-6">Counselling Scheduled</h2>
+          <div className="flex justify-center items-center gap-40">
+            <div className="flex gap-4 items-center text-lg font-bold">
+              <p>Monday 26 January 2025</p>
+              <p>11:30 AM - 12:30 PM GST</p>
+            </div>
+            <div>
+              <span className="text-xl">Admission Support</span>
+            </div>
+          </div>
+        </div>
         <Schedule />
       </div>
     </div>
